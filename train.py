6a48ee37cf8d8ab7b9861ce96e6b0bb8a3ed257f
weiyezhimeng <90603433+weiyezhimeng@users.noreply.github.com>
2024-02-06 09:33:53 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,13 +3,13 @@ from utils import loss
 import gc
 from GPU import GPU
 def train(tokenizer,model,model_bert,device,lr,EPOCH,loader,batch):
+    # ========== setup optimizer and scheduler ========== #
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),lr=lr,weight_decay=0,eps=1e-8)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 150)
     for epoch in range(EPOCH):
         step = 0
         print('Epoch:', epoch + 1, 'Training...')
         for step, (history, label) in enumerate(loader):  # 每一步loader释放一小批数据用来学习
-            # ========== setup optimizer and scheduler ========== #
-            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),lr=lr,weight_decay=0,eps=1e-8)
-            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 150)
             loss_all=loss(history,label,batch,model,model_bert,tokenizer,device)
             print("loss:", loss_all, "step:", step)
 
